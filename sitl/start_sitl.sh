33a8eef14d403cebdcb74c78305ea07b0e0d923b
hiroshi-uchihori <hiromx5@nifty.com>
2026-05-30 15:09:59 +0900

    Add Gazebo iris SITL connection test

--- sitl/start_sitl.sh
+++ sitl/start_sitl.sh
@@ -8,9 +8,16 @@ set -e
 cd /ardupilot
 
 # SITL 起動（MAVProxyなし、sim_vehicle.sh互換モード）
-sim_vehicle.py -L Ioujima \
-  -v Rover \
-  -f Boat \
-  --no-rebuild "$@" \
-  -C --no-mavproxy \
-  --out=udp:mavros:14553 
+#sim_vehicle.py -L Ioujima \
+#  -v Rover \
+#  -f Boat \
+#  --no-rebuild "$@" \
+#  -C --no-mavproxy \
+#  --out=udp:mavros:14553 
+sim_vehicle.py \
+  -v ArduCopter \
+  -f gazebo-iris \
+  --model JSON \
+  --no-rebuild \
+  --no-mavproxy \
+  --out=udp:127.0.0.1:14550
